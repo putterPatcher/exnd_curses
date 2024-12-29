b238c48d4589a38765fbaae84a07e39f6666d23e
shantanu-kor <kor.shantanu1@gmail.com>
2024-12-29 13:24:58 +0530

    changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="exnd-curses",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple use of curses.",
     author="Shantanu Kor",
     author_email="kor.shantanu1@gmail.com",
